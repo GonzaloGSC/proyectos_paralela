f04ada59dcdd49e03d36dcb2af90a1fa2a8faacf
ignaciovaldesc <63922106+ignaciovaldesc@users.noreply.github.com>
2020-06-18 16:25:05 -0400

    Reparado
    Servidor funcional entra usuario y contraseña verifica si es valido y retorna un string

--- soap/servidor.py
+++ soap/servidor.py
@@ -4,16 +4,13 @@ from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 
 
-class HelloWorldService(ServiceBase):
+class servicios(ServiceBase):
     @rpc(Unicode, Integer, _returns=Unicode)
-    def say_hello(ctx, name, times):
-        """Docstrings for service methods appear as documentation in the wsdl.
-        <b>What fun!</b>
-        @param name the name to say hello to
-        @param times the number of times to say hello
-        @return the completed array
-        """
-        if (name == 'usuario') and (times == 12345):
+    def validarusuario(ctx, name, con):
+        
+        usuario= "usuario"
+        contra = 12345
+        if (name == usuario and con == contra):
             valido = "valido"
             return valido
         else:
@@ -24,7 +21,7 @@ class HelloWorldService(ServiceBase):
         #    yield u'Hello, %s' % name
 
 
-application = Application([HelloWorldService], 'spyne.examples.hello.soap',
+application = Application([servicios], 'spyne.servicio.soap',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 
