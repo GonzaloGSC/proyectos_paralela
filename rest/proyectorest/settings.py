47e3d972d805f336c03186d19fc3882e9013d62a
= <=>
2020-07-13 00:00:57 -0400

    peticion get de datos en base

--- rest/proyectorest/settings.py
+++ rest/proyectorest/settings.py
@@ -29,7 +29,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'puntajes',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'proyectorest.urls'
--- rest/puntajes/models.py
+++ rest/puntajes/models.py
@@ -2,5 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-
-# class postulante(models.Model):
+class postulante(models.Model):
+    Nombre = models.CharField(max_length = 50)
+    Codigo = models.IntegerField()
+    Nem = models.DecimalField(max_digits = 5, decimal_places = 2)
+    Ranking = models.DecimalField(max_digits = 5, decimal_places = 2)
+    Lenguaje = models.DecimalField(max_digits = 5, decimal_places = 2)
+    Matematicas = models.DecimalField(max_digits = 5, decimal_places = 2)
+    Historia = models.DecimalField(max_digits = 5, decimal_places = 2)
+    Ciencias = models.DecimalField(max_digits = 5, decimal_places = 2)
+    PuntajePromedio = models.DecimalField(max_digits = 5, decimal_places = 2)
+    PuntajeMinimo = models.DecimalField(max_digits = 5, decimal_places = 2)
+    Vacantes = models.IntegerField()
+    PrimerMatriculado = models.DecimalField(max_digits = 5, decimal_places = 2)
+    UltimoMatriculado = models.DecimalField(max_digits = 5, decimal_places = 2)
