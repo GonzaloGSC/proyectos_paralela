15261ccfcc42b714bfe0c28c7e106b8fa8d68ebc
= <=>
2020-08-05 18:02:40 -0400

    mmm

--- rest/proyectorest/urls.py
+++ rest/proyectorest/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     #path('api/v0.01/', include('puntajes.urls')),
     path('api/v0.01/codigo',views.buscarcodigo.as_view()),
     path('api/v0.01/autor',views.autoresF.as_view()),
-    path('api/v0.01/search',views.CarrerasListView.as_view()),
+    #path('api/v0.01/search',views.CarrerasListView.as_view()),
     path('api/v0.01/carrera',views.buscarcarrera.as_view()),
     path('api/v0.01/postular',views.postular.as_view()),
 
